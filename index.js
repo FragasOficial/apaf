9bdf3987a82dc35af0f8278fe3a32b9d51045bd2
Fragas <141639158+FragasOficial@users.noreply.github.com>
2025-06-04 20:58:45 -0300

    Estrutura
    html
    -css
    -js

---
+++ index.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Apenas um exemplo de script simples para console
+console.log("Bem-vindo à APAF - Associação dos Protetores de Animais de Frecheirinha - CE!");
+
+// Aqui você pode adicionar futuras funcionalidades, como formulário de contato, animações, etc.
